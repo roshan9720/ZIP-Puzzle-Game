fa905ccd2f242b939a3afadf0260e98077f9b792
Roshan Patode <roshanpatode22@gmail.com>
2026-08-12 13:25:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-Document.addEventListener('DOMContentLoaded', () => {
-let coins = parseInt(localStorage.getItem('zip_coins'));
-if (isNaN(coins)) {
-    coins = 1000; // 500 default + 500 added
-    localStorage.setItem('zip_coins', coins);
-} else if (!localStorage.getItem('zip_500_added')) {
-    coins += 500;
-    localStorage.setItem('zip_500_added', 'true');
-    localStorage.setItem('zip_coins', coins);
-}
-
+document.addEventListener('DOMContentLoaded', () => {
+let coins = parseInt(localStorage.getItem('zip_coins')) || 500;
 let currentLevel = parseInt(localStorage.getItem('zip_current_level')) || 1;
 let highestUnlocked = parseInt(localStorage.getItem('zip_highest_unlocked')) || 1;
 let savedPaths = JSON.parse(localStorage.getItem('zip_saved_paths') || '{}');
@@ -127,4 +118,5 @@ if (lvl <= 3) {
 let numCheckpoints = Math.floor(rand() * (maxNumbers - minNumbers + 1)) + minNumbers;  
 
 const checkpoints = {};
-});
+
+Ye hai js kaha edit kraru
